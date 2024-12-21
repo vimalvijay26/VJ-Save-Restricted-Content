162d5ded6e00d29f2462ffa55f5363c9bbda7e71
vimalvijay26 <93144424+vimalvijay26@users.noreply.github.com>
2024-12-22 04:34:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,15 @@
+
+
 import os
 
-# Bot token @Botfather
+# Bot token @BotFather
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "22182189"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
 
-# Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "6073523936"))
-
-# Your Mongodb Database Url
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
-
-# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+# Database
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://username:password@cluster0.mongodb.net/?retryWrites=true&w=majority")
